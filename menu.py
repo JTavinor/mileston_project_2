0abeb579a0b057b969af990dad669de679d1fa14
JTavinor <59698353+JTavinor@users.noreply.github.com>
2020-01-09 15:14:04 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -8,7 +8,6 @@ Search your library? (s);
 Add a book to your library? (a);
 Mark a book in your library as read? (r);
 Delete a book from your library? (d);
-Add a new library? (n);
 Or exit the app? (q): """)
         if user_input == 'v':
             app.view_books()
@@ -21,8 +20,6 @@ Or exit the app? (q): """)
             app.read_book()
         if user_input == 'd':
             app.delete_book()
-        if user_input == 'n':
-            app.add_library()
         if user_input == 'q':
             print('Goodbye!')
             break
